31cca30ee8d587bc478471b1d3df71ebe7db6baf
Edwin Hernandez <Edwinhern.16@gmail.com>
2021-07-19 15:13:56 -0500

    added website functions (#2)

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-#Comment for first commit test
-#comment for testing JIRA
-#comment for testing slack integration
+# Comment for first commit test
+# comment for testing JIRA
+# comment for testing slack integration
+from flask import Flask, render_template, url_for, flash, redirect, request
+
+# this gets the name of the file so Flask knows it's name
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '2d6ca153ea201fe4daf5a90f380026b5'
+
+
+# Homepage
+@app.route("/")
+def home():
+    return render_template("home.html", subtitle='Home Page')
+
+
+# Main Function
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
