a1c9d9b0989748ea1a6e3e065070baaa840bee5c
Jacob Gil <jacobgil325@gmail.com>
2021-07-27 16:58:41 +0000

    adding tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,3 +7,9 @@ from main import app as flask_app  ##
 def app():
       app = flask_app
       return app
+    
+@pytest.fixture
+def client():
+    app = flask_app
+    client = app.test_client()
+    return client
