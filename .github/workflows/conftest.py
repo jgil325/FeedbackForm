950d1e23cc3d357041e66a8a89bb1e06197936a1
Jacob Gil <jacobgil325@gmail.com>
2021-07-27 16:01:16 +0000

    adding tests

--- .github/workflows/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from main import app as flask_app  ##
-
-
-@pytest.fixture
-def app()
--- .github/workflows/test_app.py
+++
@@ -1,3 +0,0 @@
-def test_index(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
